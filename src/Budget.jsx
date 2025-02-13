fa27028eaa161e08ec58f969903812afe72b901e
Dawlabani <64708428+Dawlabani@users.noreply.github.com>
2025-02-13 09:41:59 +0200

    Main files commit

---
+++ src/Budget.jsx
@@ -0,0 +1,12 @@
+// Budget.jsx
+import React from 'react';
+
+function Budget() {
+  return (
+    <div>
+      <h1>Budget Management (Coming Soon)</h1>
+    </div>
+  );
+}
+
+export default Budget;
---
+++ src/Profile.jsx
@@ -0,0 +1,12 @@
+// Profile.jsx
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile (Coming Soon)</h1>
+    </div>
+  );
+}
+
+export default Profile;
