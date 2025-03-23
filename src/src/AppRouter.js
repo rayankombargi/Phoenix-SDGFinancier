e82789706186354a41e98947b51387bb1f506cde
mounirkaissi <159577885+mounirkaissi@users.noreply.github.com>
2025-03-23 15:49:01 +0200

    AppRouter.js
    Added the routes to the sign in and sign up pages

--- src/src/AppRouter.js
+++ src/src/AppRouter.js
@@ -1,10 +1,12 @@
 // src/AppRouter.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './HomePage';
-import Dashboard from './Dashboard/Dashboard';
-import Budget from './Budget/BudgetPage';
-import Profile from './Profile/ProfilePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./HomePage";
+import Dashboard from "./Dashboard/Dashboard";
+import Budget from "./Budget/BudgetPage";
+import Profile from "./Profile/ProfilePage";
+import Signin from "./Profile/SignInForm";
+import Signup from "./Profile/SignUpForm";
 
 function AppRouter() {
   return (
@@ -14,6 +16,8 @@ function AppRouter() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/budget" element={<Budget />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Signin />} />
       </Routes>
     </Router>
   );
